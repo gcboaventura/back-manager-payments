23e2b11d1fe9b6b2a209d1ad4c3749bbceed7128
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 13:15:56 -0300

    feat: pase int port db and create env db database

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -6,5 +6,6 @@ export default {
 	DB_HOST: process.env.DB_HOST || '',
 	DB_USER: process.env.DB_USER || '',
 	DB_PASSWORD: process.env.DB_PASSWORD || '',
-	DB_PORT: process.env.DB_PORT || ''
+	DB_PORT: parseInt(process.env.DB_PORT || '3306'),
+	DB_DATABASE: process.env.DB_DATABASE || ''
 }
