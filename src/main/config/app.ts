86867982a8d01264a62714f6ba954c0e8ca80889
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 16:39:01 -0300

    feat: configure middlewares

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import setupRoutes from './routes'
+import setupMiddlewares from './middlewares'
 
 const app = express()
 
 dotenv.config()
 
 setupRoutes(app)
+
+setupMiddlewares(app)
 
 export default app
