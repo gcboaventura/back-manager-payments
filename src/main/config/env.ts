01a76b90305145d9e6b025a37f75a7dacb632160
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 13:11:52 -0300

    feat: add envs mysql

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,9 @@ export default {
 	PORT: process.env.PORT || 4000,
 	PAGARME_SECRET_KEY: process.env.PAGARME_SECRTET_KEY || '',
 	PAGARME_PUBLIC_KEY: process.env.PAGARME_PUBLIC_KEY || '',
-	PAGARME_ENDPOINT: process.env.PAGARME_ENDPOINT || ''
+	PAGARME_ENDPOINT: process.env.PAGARME_ENDPOINT || '',
+	DB_HOST: process.env.DB_HOST || '',
+	DB_USER: process.env.DB_USER || '',
+	DB_PASSWORD: process.env.DB_PASSWORD || '',
+	DB_PORT: process.env.DB_PORT || ''
 }
