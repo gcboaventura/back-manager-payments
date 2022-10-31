45a16aeca2a7529a2813cfe21c44bd18abd1ec91
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 20:26:27 -0300

    feat: add plan infra layer

--- src/infra/pagar-me/plans/add.ts
+++ src/infra/pagar-me/plans/add.ts
@@ -1,14 +1,10 @@
 import { AddPlanInfra } from '../../../data/protocols'
 import { PlanEntity } from '../../../domain'
-import { env } from '../../../main/config'
-import pagarme from 'pagarme'
+import { http } from '../../axios'
 
 export class AddPlanPagarme implements AddPlanInfra {
 	async add(plan: PlanEntity): Promise<any> {
-		const client = await pagarme.client.connect({ api_key: env.PAGARME_SECRET_KEY })
-
-		const insert = await client.plans.create(undefined, { ...plan })
-
-		return insert
+		const { data } = await http.post('/plans', plan)
+		return data
 	}
 }
