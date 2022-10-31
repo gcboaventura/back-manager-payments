9938e605181dedc014a4981a7f95092fc87eac87
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 11:02:20 -0300

    refactor: rename key and add public key

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
 	PORT: process.env.PORT || 4000,
-	PAGARME_KEY: process.env.PAGARME_KEY || '',
+	PAGARME_SECRET_KEY: process.env.PAGARME_SECRTET_KEY || '',
+	PAGARME_PUBLIC_KEY: process.env.PAGARME_PUBLIC_KEY || '',
 	PAGARME_ENDPOINT: process.env.PAGARME_ENDPOINT || ''
 }
