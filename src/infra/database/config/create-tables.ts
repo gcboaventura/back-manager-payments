7da0a6b154af6435b870367a05510c77fb2272d1
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 15:00:10 -0300

    feat: configure table client

--- src/infra/database/config/create-tables.ts
+++ src/infra/database/config/create-tables.ts
@@ -12,11 +12,11 @@ export class CreateTable implements TableModels {
 	client(): void {
 		this.connection.query(
 			`CREATE TABLE IF NOT EXISTS CLIENTS (
-					id INT AUTO_INCREMENT PRIMARY KEY,
-					id_pagarme VARCHAR(255),
+					id_gateway VARCHAR(255),
 					email VARCHAR(255),
 					plan INT,
 					due_date DATE,
+					card_token VARCHAR(255),
 					created_at DATETIME,
 					updated_at DATETIME,
           UNIQUE(email)
