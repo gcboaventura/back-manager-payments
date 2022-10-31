320100be24549d1e1e3b729f7c6d187ad8b5677c
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 16:23:22 -0300

    feat: adapters

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import { Controller, HttpRequest } from '../../presentation/protocols'
+
+export const adaptRoute = (controller: Controller) => {
+	return async (request: Request, response: Response) => {
+		const httpRequest: HttpRequest = {
+			body: request.body,
+			params: request.params,
+			query: request.query,
+			headers: request.headers
+		}
+		const httpResponse = await controller.handle(httpRequest)
+		response.status(httpResponse.statusCode).json(httpResponse.body)
+	}
+}
