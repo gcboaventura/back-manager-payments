9a6ca1c78a03566d69ea2099e923f628ac280e16
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 17:34:41 -0300

    refactor: update name env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
 	PORT: process.env.PORT || 4000,
-	PAGARME_SECRET_KEY: process.env.PAGARME_SECRTET_KEY || '',
+	PAGARME_SECRET_KEY: process.env.PAGARME_SECRET_KEY || '',
 	PAGARME_PUBLIC_KEY: process.env.PAGARME_PUBLIC_KEY || '',
 	PAGARME_ENDPOINT: process.env.PAGARME_ENDPOINT || '',
 	DB_HOST: process.env.DB_HOST || '',
@@ -9,3 +9,5 @@ export default {
 	DB_PORT: parseInt(process.env.DB_PORT || '3306'),
 	DB_DATABASE: process.env.DB_DATABASE || ''
 }
+
+console.log(process.env.PAGARME_PUBLIC_KEY)
