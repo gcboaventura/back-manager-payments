fb97b94f9f7c184017c333c49bde603feec00303
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-10-31 10:59:14 -0300

    feat: pagarme endpoint

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
 	PORT: process.env.PORT || 4000,
-	PAGARME_KEY: process.env.PAGARME_KEY || ''
+	PAGARME_KEY: process.env.PAGARME_KEY || '',
+	PAGARME_ENDPOINT: process.env.PAGARME_ENDPOINT || ''
 }
