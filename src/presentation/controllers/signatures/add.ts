816183322cc7a6ee779431273a59cacf4294a61b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-01 16:16:32 -0300

    feat: add signature controller

---
+++ src/presentation/controllers/signatures/add.ts
@@ -0,0 +1,30 @@
+import { AddSignatureUseCase } from '../../../domain'
+import { MissingParamError } from '../../errors'
+import { badRequest, serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class AddSignatureController implements Controller {
+	private readonly addSignatureUseCase: AddSignatureUseCase
+
+	constructor(addSignatureUseCase: AddSignatureUseCase) {
+		this.addSignatureUseCase = addSignatureUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const requiredFields: string[] = ['plan_id', 'payment_method', 'customer_id', 'card']
+
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field))
+				}
+			}
+
+			const signature = await this.addSignatureUseCase.add(httpRequest.body)
+
+			return success(signature)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
