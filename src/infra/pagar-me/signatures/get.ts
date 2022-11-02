0112d995a1d1a532d012359f0ef4fdd9a41cb5ed
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-01 21:22:33 -0300

    refactor: change method http axios

--- src/infra/pagar-me/signatures/get.ts
+++ src/infra/pagar-me/signatures/get.ts
@@ -4,7 +4,7 @@ import { http } from '../../axios'
 
 export class GetSignaturePagarme implements GetSignatureInfra {
 	async get(id: string): Promise<SignatureModel> {
-		const { data } = await http.post(`/subscriptions/${id}`)
+		const { data } = await http.get(`/subscriptions/${id}`)
 		return data
 	}
 }
