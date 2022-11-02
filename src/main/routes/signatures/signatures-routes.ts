493c8eba30a9e58e18c5ec577d20fc176a9e6e13
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-01 21:14:25 -0300

    feat: get signature route

--- src/main/routes/signatures/signatures-routes.ts
+++ src/main/routes/signatures/signatures-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../../adapters'
-import { addSignatureFatory } from '../../factories/signtatures'
+import { addSignatureFatory, getSignatureFatory } from '../../factories/signtatures'
 
 export default (router: Router): void => {
 	router.post('/add-signature', adaptRoute(addSignatureFatory()))
+
+	router.post('/get-signature/:id', adaptRoute(getSignatureFatory()))
 }
