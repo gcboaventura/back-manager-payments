87be3127a8516bea09a0f4f54dc310a1c3d2f6e0
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-01 21:39:36 -0300

    feat: delete signature route

--- src/main/routes/signatures/signatures-routes.ts
+++ src/main/routes/signatures/signatures-routes.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
 import { adaptRoute } from '../../adapters'
-import { addSignatureFatory, getSignatureFatory } from '../../factories/signtatures'
+import {
+	addSignatureFatory,
+	getSignatureFatory,
+	deleteSignatureFatory
+} from '../../factories/signtatures'
 
 export default (router: Router): void => {
 	router.post('/add-signature', adaptRoute(addSignatureFatory()))
 
 	router.get('/get-signature/:id', adaptRoute(getSignatureFatory()))
+
+	router.delete('/delete-signature/:id', adaptRoute(deleteSignatureFatory()))
 }
