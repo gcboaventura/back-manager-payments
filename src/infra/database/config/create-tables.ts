76f686bf0085d1fa6ac6a9e2058bd50a893d434e
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-04 14:07:49 -0300

    refactor: structure table client

--- src/infra/database/config/create-tables.ts
+++ src/infra/database/config/create-tables.ts
@@ -13,10 +13,7 @@ export class CreateTable implements TableModels {
 		this.connection.query(
 			`CREATE TABLE IF NOT EXISTS CLIENTS (
 				  id_local INT PRIMARY KEY AUTO INCREMENT,
-					id_gateway VARCHAR(255),
-					id_plan VARCHAR(255),
 					id_signature VARCHAR(255),
-					id_credit_card VARCHAR(255),
 					created_at DATETIME,
 					updated_at DATETIME,
 			)`
