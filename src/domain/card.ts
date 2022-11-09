dded45e76f46b04d4cb0b711371e052ae65d2f5b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-09 15:23:16 -0300

    feat: update domain card

--- src/domain/card.ts
+++ src/domain/card.ts
@@ -1,6 +1,7 @@
-import { Address } from './address'
+import { CustomerModel } from './customer'
 
 export interface Card {
+	customer_id: string
 	number: string
 	holder_name: string
 	exp_month: number
@@ -13,6 +14,67 @@ export interface Card {
 	billing_address: Address
 }
 
-export interface CardUpdate extends Card {
-	card_id: string
+export interface CardModel extends Card {
+	id: string
+	first_six_digits: string
+	last_four_digits: string
+	status: string
+	created_at: string
+	updated_at: string
+	customer: CustomerModel
+	type: string
+}
+
+export interface Address {
+	id?: string
+	country: string
+	state: string
+	city: string
+	zip_code: string
+	line_1: string
+	line_2: string
+}
+
+export interface TokenCard {
+	type: string
+	card: Card
+	appId: string
+}
+
+export interface TokenCardModel extends Card {
+	id: string
+	first_six_digits: string
+	last_four_digits: string
+	status: string
+	created_at: string
+	updated_at: string
+	type: string
+}
+
+export interface AddCardUseCase {
+	add(card: Card): Promise<CardModel>
+}
+
+export interface GetCardUseCase {
+	get(customer_id: string, card_id: string): Promise<CardModel>
+}
+
+export interface ListCardsUseCase {
+	list(customer_id: string): Promise<CardModel[]>
+}
+
+export interface UpdateCardUseCase {
+	update(card: CardModel): Promise<CardModel>
+}
+
+export interface DeleteCardUseCase {
+	delete(customer_id: string, card_id: string): Promise<CardModel>
+}
+
+export interface RenewCardUseCase {
+	renew(customer_id: string, card_id: string): Promise<CardModel>
+}
+
+export interface TokenizeCardUseCase {
+	tokenize(card: TokenCard): Promise<TokenCardModel>
 }
