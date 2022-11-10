4ac72b4d92eac9762637d50cab576db604eec69d
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-10 20:20:53 -0300

    feat: get customer pagarme

---
+++ src/infra/pagar-me/customer/get.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { GetCustomerGateway } from '../../../data'
+import { CustomerModel } from '../../../domain'
+
+export class GetCustomerPagarme implements GetCustomerGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async get(id: string): Promise<CustomerModel> {
+		const { data } = await this.axios.get(`/customers/${id}`)
+		return data
+	}
+}
