906d3133f66982051b8f8eac568deec0791c4640
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-10 20:23:01 -0300

    feat: get customer factory

---
+++ src/main/factories/customer/get.ts
@@ -0,0 +1,11 @@
+import { GetCustomerData } from '../../../data'
+import { GetCustomerController } from '../../../presentation'
+import { GetCustomerPagarme, AXIOS } from '../../../infra'
+
+export const getCustomerFactory = (): GetCustomerController => {
+	const getCustomerGateway = new GetCustomerPagarme(AXIOS)
+
+	const GetCustomerUseCase = new GetCustomerData(getCustomerGateway)
+
+	return new GetCustomerController(GetCustomerUseCase)
+}
