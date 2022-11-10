661fbb6c82896b261afa921e2e0a9f31a442d594
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-10 20:23:39 -0300

    feat: get customer route

--- src/main/routes/customer-routes.ts
+++ src/main/routes/customer-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addCustomerFactory } from '../factories'
+import { addCustomerFactory, getCustomerFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/customers', adaptRoute(addCustomerFactory()))
+
+	router.get('/customers/:id', adaptRoute(getCustomerFactory()))
 }
