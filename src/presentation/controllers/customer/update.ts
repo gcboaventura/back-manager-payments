d6934f51e95a097d2c7e070aa385c9b7a216537d
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-10 20:29:43 -0300

    feat: update customer controller

---
+++ src/presentation/controllers/customer/update.ts
@@ -0,0 +1,21 @@
+import { UpdateCustomerUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class UpdateCustomerController implements Controller {
+	private readonly updateCustomerUseCase: UpdateCustomerUseCase
+
+	constructor(updateCustomerUseCase: UpdateCustomerUseCase) {
+		this.updateCustomerUseCase = updateCustomerUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const customer = await this.updateCustomerUseCase.update(httpRequest.body)
+
+			return success(customer)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
