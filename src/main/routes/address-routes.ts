2f966a1a0312cd0656943dbc7dbfbb7bf94ea6f2
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 11:55:41 -0300

    feat: address routes

---
+++ src/main/routes/address-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters'
+import { addAddressFactory } from '../factories'
+
+export default (router: Router): void => {
+	router.post('customers/:id/addresses', adaptRoute(addAddressFactory()))
+}
