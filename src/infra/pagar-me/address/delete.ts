93a47988dcec50dabfd73d736ccc8daacd792377
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:59:10 -0300

    feat: delete address pagarme

---
+++ src/infra/pagar-me/address/delete.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { DeleteAddressGateway } from '../../../data'
+import { AddressModel } from '../../../domain'
+
+export class DeleteAddressPagarme implements DeleteAddressGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async delete(customer_id: string, address_id: string): Promise<AddressModel> {
+		const { data } = await this.axios.delete(`/customers/${customer_id}/addresses/${address_id}`)
+		return data
+	}
+}
