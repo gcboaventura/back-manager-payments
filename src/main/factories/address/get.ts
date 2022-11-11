f5f4bf4d2f9df8b5a68d77b2f59780c3de926629
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:29:14 -0300

    feat: get address main layer

---
+++ src/main/factories/address/get.ts
@@ -0,0 +1,11 @@
+import { GetAddressData } from '../../../data'
+import { GetAddressController } from '../../../presentation'
+import { GetAddressPagarme, AXIOS } from '../../../infra'
+
+export const getAddressFactory = (): GetAddressController => {
+	const getAddressGateway = new GetAddressPagarme(AXIOS)
+
+	const getAddressUseCase = new GetAddressData(getAddressGateway)
+
+	return new GetAddressController(getAddressUseCase)
+}
--- src/main/routes/address-routes.ts
+++ src/main/routes/address-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addAddressFactory } from '../factories'
+import { addAddressFactory, getAddressFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/customers/:id/addresses', adaptRoute(addAddressFactory()))
+
+	router.get('/customers/:idCustomer/addresses/:idAddress', adaptRoute(getAddressFactory()))
 }
