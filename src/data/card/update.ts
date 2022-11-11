e689b37a7badb9c078accbc04cc47976a184a2ab
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 11:13:05 -0300

    feat: update card data layer

---
+++ src/data/card/update.ts
@@ -0,0 +1,20 @@
+import { UpdateCardUseCase, Card, CardModel } from '../../domain'
+import { UpdateCardGateway, UpdateCardRepository } from '../protocols'
+
+export class UpdateCardData implements UpdateCardUseCase {
+	private readonly updateCardGateway: UpdateCardGateway
+	private readonly updateCardRepository: UpdateCardRepository
+
+	constructor(updateCardGateway: UpdateCardGateway, updateCardRepository: UpdateCardRepository) {
+		this.updateCardGateway = updateCardGateway
+		this.updateCardRepository = updateCardRepository
+	}
+
+	async update(customer_id: string, card_id: string, card: Card): Promise<CardModel> {
+		const updateGateway = await this.updateCardGateway.update(customer_id, card_id, card)
+
+		await this.updateCardRepository.update(updateGateway)
+
+		return updateGateway
+	}
+}
---
+++ src/data/protocols/card/update.ts
@@ -0,0 +1,7 @@
+import { CardModel, UpdateCardUseCase } from '../../../domain'
+
+export interface UpdateCardGateway extends UpdateCardUseCase {}
+
+export interface UpdateCardRepository {
+	update(card: CardModel): Promise<CardModel>
+}
