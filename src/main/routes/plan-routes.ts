77663a1363a24969c7a8e7df65fbd0fae27c47cb
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:27:47 -0300

    feat: get planr route

--- src/main/routes/plan-routes.ts
+++ src/main/routes/plan-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addPlanFactory } from '../factories'
+import { addPlanFactory, getPlanFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/plans', adaptRoute(addPlanFactory()))
+
+	router.post('/plans/:id', adaptRoute(getPlanFactory()))
 }
