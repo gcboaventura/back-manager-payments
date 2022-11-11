cc10b140226c0356db1e60acde292109bd9b4721
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 11:31:02 -0300

    feat: delete card pagarme

---
+++ src/infra/pagar-me/card/delete.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { DeleteCardGateway } from '../../../data'
+import { CardModel } from '../../../domain'
+
+export class DeleteCardPagarme implements DeleteCardGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async delete(customer_id: string, card_id: string): Promise<CardModel> {
+		const { data } = await this.axios.delete(`/customers/${customer_id}/cards/${card_id}`)
+		return data
+	}
+}
