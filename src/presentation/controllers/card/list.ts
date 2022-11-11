8cdd3bcd8a86fe46b2a9f3ccfae5762ba55b647b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 10:59:30 -0300

    feat: list card controller

---
+++ src/presentation/controllers/card/list.ts
@@ -0,0 +1,23 @@
+import { ListCardsUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class ListCardsController implements Controller {
+	private readonly listCardsUseCase: ListCardsUseCase
+
+	constructor(listCardsUseCase: ListCardsUseCase) {
+		this.listCardsUseCase = listCardsUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const { id } = httpRequest.params
+
+			const cardList = await this.listCardsUseCase.list(id)
+
+			return success(cardList)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
