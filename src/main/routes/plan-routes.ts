b4c21d1082801ba9ece1d70a2114363b15a05105
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:43:00 -0300

    feat: update plan route

--- src/main/routes/plan-routes.ts
+++ src/main/routes/plan-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addPlanFactory, getPlanFactory } from '../factories'
+import { addPlanFactory, getPlanFactory, updatePlanFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/plans', adaptRoute(addPlanFactory()))
 
 	router.get('/plans/:id', adaptRoute(getPlanFactory()))
+
+	router.put('/plans/:id', adaptRoute(updatePlanFactory()))
 }
