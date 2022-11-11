f2f6c181d82764d99a3194563cd97b5f47cfed90
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:35:26 -0300

    feat: update plan data layer

---
+++ src/data/plan/update.ts
@@ -0,0 +1,20 @@
+import { UpdatePlanUseCase, Plan, PlanModel } from '../../domain'
+import { UpdatePlanGateway, UpdatePlanRepository } from '../protocols'
+
+export class UpdatePlanData implements UpdatePlanUseCase {
+	private readonly updatePlanGateway: UpdatePlanGateway
+	private readonly updatePlanRepository: UpdatePlanRepository
+
+	constructor(updatePlanGateway: UpdatePlanGateway, updatePlanRepository: UpdatePlanRepository) {
+		this.updatePlanGateway = updatePlanGateway
+		this.updatePlanRepository = updatePlanRepository
+	}
+
+	async update(plan: Plan, plan_id: string): Promise<PlanModel> {
+		const updateGateway = await this.updatePlanGateway.update(plan, plan_id)
+
+		await this.updatePlanRepository.update(updateGateway)
+
+		return updateGateway
+	}
+}
---
+++ src/data/protocols/plan/update.ts
@@ -0,0 +1,7 @@
+import { PlanModel, UpdatePlanUseCase } from '../../../domain'
+
+export interface UpdatePlanGateway extends UpdatePlanUseCase {}
+
+export interface UpdatePlanRepository {
+	update(plan: PlanModel): Promise<PlanModel>
+}
