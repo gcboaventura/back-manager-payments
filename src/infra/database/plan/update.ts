a663f3da83264276bb0c74835df7c75e37509ef8
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:40:52 -0300

    feat: update plan repository

---
+++ src/infra/database/plan/update.ts
@@ -0,0 +1,55 @@
+import { Connection } from 'mysql'
+import { UpdatePlanRepository } from '../../../data'
+import { PlanModel } from '../../../domain'
+import { DateModel } from '../../../utils/date/types'
+
+export class UpdatePlanMysql implements UpdatePlanRepository {
+	private readonly connection: Connection
+	private readonly dateUtils: DateModel
+
+	constructor(connection: Connection, dateUtils: DateModel) {
+		this.connection = connection
+		this.dateUtils = dateUtils
+	}
+
+	async update(plan: PlanModel): Promise<PlanModel> {
+		const {
+			id,
+			name,
+			description,
+			statement_descriptor,
+			minimum_price,
+			interval,
+			billing_type,
+			payment_methods,
+			currency,
+			updated_at,
+			status
+		} = plan
+
+		return new Promise((resolve, reject) => {
+			this.connection.query(
+				`UPDATE PLANS SET name = ?, description = ?, statement_descriptor = ?, minimum_price = ?, interval_plan = ?, billing_type = ?, payment_methods = ?, status = ?, currency = ?, updated_at  = ? WHERE id_gateway = ?`,
+				[
+					name,
+					description,
+					statement_descriptor,
+					minimum_price,
+					interval,
+					billing_type,
+					payment_methods,
+					status,
+					currency,
+					this.dateUtils.insertDb(updated_at),
+					id
+				],
+				(error: any, res: any) => {
+					if (error) {
+						reject(error)
+					}
+					resolve(res)
+				}
+			)
+		})
+	}
+}
