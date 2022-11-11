2156d0cee0bce08cd29cd98db32ccb3646838388
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:22:24 -0300

    feat: get plan controller

---
+++ src/presentation/controllers/plan/get.ts
@@ -0,0 +1,21 @@
+import { GetPlanUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class GetPlanController implements Controller {
+	private readonly getPlanUseCase: GetPlanUseCase
+
+	constructor(getPlanUseCase: GetPlanUseCase) {
+		this.getPlanUseCase = getPlanUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const plan = await this.getPlanUseCase.get(httpRequest.body)
+
+			return success(plan)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
