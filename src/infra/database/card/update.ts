6c667b54158ef545d612f22be8b863649d888cd9
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 11:19:25 -0300

    feat: update card repository

---
+++ src/infra/database/card/update.ts
@@ -0,0 +1,57 @@
+import { Connection } from 'mysql'
+import { UpdateCardRepository } from '../../../data'
+import { CardModel } from '../../../domain'
+import { DateModel } from '../../../utils/date/types'
+
+export class UpdateCardMysql implements UpdateCardRepository {
+	private readonly connection: Connection
+	private readonly dateUtils: DateModel
+
+	constructor(connection: Connection, dateUtils: DateModel) {
+		this.connection = connection
+		this.dateUtils = dateUtils
+	}
+
+	async update(card: CardModel): Promise<CardModel> {
+		const {
+			id,
+			first_six_digits,
+			last_four_digits,
+			brand,
+			holder_name,
+			holder_document,
+			exp_month,
+			exp_year,
+			status,
+			label,
+			type,
+			updated_at
+		} = card
+
+		return new Promise((resolve, reject) => {
+			this.connection.query(
+				`UPDATE CUSTOMERS SET first_six_digits = ?, last_four_digits = ?, brand = ?, holder_name = ?, holder_document = ?, exp_month = ?, exp_year = ?, status = ?, label = ?, type = ?, updated_at = ? WHERE id_gateway = ?`,
+				[
+					first_six_digits,
+					last_four_digits,
+					brand,
+					holder_name,
+					holder_document,
+					exp_month,
+					exp_year,
+					status,
+					label,
+					type,
+					this.dateUtils.insertDb(updated_at),
+					id
+				],
+				(error: any, res: any) => {
+					if (error) {
+						reject(error)
+					}
+					resolve(res)
+				}
+			)
+		})
+	}
+}
