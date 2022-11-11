dfc3295cc4102a5020b9333a8991fb228ac60070
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:37:03 -0300

    feat: update address pagarme

---
+++ src/infra/pagar-me/address/update.ts
@@ -0,0 +1,23 @@
+import { Axios } from 'axios'
+import { UpdateAddressGateway } from '../../../data'
+import { AddressModel, AddressUpdate } from '../../../domain'
+
+export class UpdateAddressPagarme implements UpdateAddressGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async update(
+		address: AddressUpdate,
+		customer_id: string,
+		address_id: string
+	): Promise<AddressModel> {
+		const { data } = await this.axios.put(
+			`/customers/${customer_id}/addresses/${address_id}`,
+			address
+		)
+		return data
+	}
+}
