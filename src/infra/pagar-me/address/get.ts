c764124eb929d3a0a9de9fde1c19945b108456b0
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:27:12 -0300

    feat: get address infra pagarme

---
+++ src/infra/pagar-me/address/get.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { GetAddressGateway } from '../../../data'
+import { AddressModel } from '../../../domain'
+
+export class GetAddressPagarme implements GetAddressGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async get(customer_id: string, address_id: string): Promise<AddressModel> {
+		const { data } = await this.axios.get(`/customers/${customer_id}/addresses/${address_id}`)
+		return data
+	}
+}
