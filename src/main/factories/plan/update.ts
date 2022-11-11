ccf75f364ef7d6a24ef8b3eebb3c882cc92d02a0
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 16:41:59 -0300

    feat: update plan factory

---
+++ src/main/factories/plan/update.ts
@@ -0,0 +1,16 @@
+import { UpdatePlanData } from '../../../data'
+import { UpdatePlanController } from '../../../presentation'
+import { UpdatePlanMysql, UpdatePlanPagarme, AXIOS, connection } from '../../../infra'
+import { DateUtils } from '../../../utils'
+
+export const updatePlanFactory = (): UpdatePlanController => {
+	const updatePlanGateway = new UpdatePlanPagarme(AXIOS)
+
+	const dateUtils = new DateUtils()
+
+	const updatePlanRepository = new UpdatePlanMysql(connection, dateUtils)
+
+	const updatePlanUseCase = new UpdatePlanData(updatePlanGateway, updatePlanRepository)
+
+	return new UpdatePlanController(updatePlanUseCase)
+}
