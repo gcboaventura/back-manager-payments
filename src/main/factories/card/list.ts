daa501f1bc6ae5c74dc135f24d28a13aa8729269
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 11:04:58 -0300

    feat: list cards main layer

---
+++ src/main/factories/card/list.ts
@@ -0,0 +1,11 @@
+import { ListCardsData } from '../../../data'
+import { ListCardsController } from '../../../presentation'
+import { ListCardsPagarme, AXIOS } from '../../../infra'
+
+export const listCardsFactory = (): ListCardsController => {
+	const listCardsGateway = new ListCardsPagarme(AXIOS)
+
+	const listCardsUseCase = new ListCardsData(listCardsGateway)
+
+	return new ListCardsController(listCardsUseCase)
+}
--- src/main/routes/card-routes.ts
+++ src/main/routes/card-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addCardFactory, getCardFactory } from '../factories'
+import { addCardFactory, getCardFactory, listCardsFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/customers/:id/cards', adaptRoute(addCardFactory()))
 
 	router.get('/customers/:idCustomer/cards/:idCard', adaptRoute(getCardFactory()))
+
+	router.get('/customers/:id/cards', adaptRoute(listCardsFactory()))
 }
