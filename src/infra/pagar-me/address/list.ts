4c9c5c8d3c1fb7b221780a67e8042958c2240ac0
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:49:20 -0300

    feat: list address infra

---
+++ src/infra/pagar-me/address/list.ts
@@ -0,0 +1,30 @@
+import { Axios } from 'axios'
+import { ListAddressGateway } from '../../../data'
+import { AddressModel, GatewayResponse, QueryListAddress } from '../../../domain'
+
+export class ListAddressPagarme implements ListAddressGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async list(
+		query: QueryListAddress,
+		customer_id: string
+	): Promise<GatewayResponse<AddressModel[]>> {
+		const params: string[] = []
+
+		const url: string = `/customers/${customer_id}/addresses`
+
+		if (query) {
+			Object.entries(query).forEach((x: string[]) =>
+				params.length === 0 ? params.push(`?${x[0]}=${x[1]}`) : params.push(`&${x[0]}=${x[1]}`)
+			)
+		}
+
+		const { data } = await this.axios.get(`${url}${params.join('')}`)
+
+		return data
+	}
+}
