f36ca640f00da15c3aa8e66a68f98bdcd02d67b2
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-11 13:41:41 -0300

    feat: update address route

--- src/main/routes/address-routes.ts
+++ src/main/routes/address-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addAddressFactory, getAddressFactory } from '../factories'
+import { addAddressFactory, getAddressFactory, updateAddressFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/customers/:id/addresses', adaptRoute(addAddressFactory()))
 
 	router.get('/customers/:idCustomer/addresses/:idAddress', adaptRoute(getAddressFactory()))
+
+	router.put('/customers/:idCustomer/addresses/:idAddress', adaptRoute(updateAddressFactory()))
 }
