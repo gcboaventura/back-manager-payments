1cdc72ebb5481ef3c5db193596858d0555524ac7
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:35:47 -0300

    feat: get signature factory

---
+++ src/main/factories/signature/get.ts
@@ -0,0 +1,11 @@
+import { GetSignatureData } from '../../../data'
+import { GetSignatureController } from '../../../presentation'
+import { GetSignaturePagarme, AXIOS } from '../../../infra'
+
+export const getSignatureFactory = (): GetSignatureController => {
+	const getSignatureGateway = new GetSignaturePagarme(AXIOS)
+
+	const getSignatureUseCase = new GetSignatureData(getSignatureGateway)
+
+	return new GetSignatureController(getSignatureUseCase)
+}
