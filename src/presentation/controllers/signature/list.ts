42605f561eb6a2a3caeba9057e0f7e4aa3ca26a6
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:38:44 -0300

    feat: list signatures controller

---
+++ src/presentation/controllers/signature/list.ts
@@ -0,0 +1,21 @@
+import { ListSignaturesUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class ListSignaturesController implements Controller {
+	private readonly listSignaturesUseCase: ListSignaturesUseCase
+
+	constructor(listSignaturesUseCase: ListSignaturesUseCase) {
+		this.listSignaturesUseCase = listSignaturesUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const signatureList = await this.listSignaturesUseCase.list(httpRequest.query)
+
+			return success(signatureList)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
