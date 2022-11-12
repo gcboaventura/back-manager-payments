2dc93fa2a7b4efd1c8b285d8f985917d54a5677d
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 17:49:37 -0300

    feat: delete item plan factory

---
+++ src/main/factories/plan/delete-item.ts
@@ -0,0 +1,19 @@
+import { DeleteItemPlanData } from '../../../data'
+import { DeleteItemPlanController } from '../../../presentation'
+import { DeleteItemPlanMysql, DeleteItemPlanPagarme, AXIOS, connection } from '../../../infra'
+import { DateUtils } from '../../../utils'
+
+export const deleteItemPlanFactory = (): DeleteItemPlanController => {
+	const deleteItemPlanGateway = new DeleteItemPlanPagarme(AXIOS)
+
+	const dateUtils = new DateUtils()
+
+	const deleteItemPlanRepository = new DeleteItemPlanMysql(connection, dateUtils)
+
+	const deleteItemPlanUseCase = new DeleteItemPlanData(
+		deleteItemPlanGateway,
+		deleteItemPlanRepository
+	)
+
+	return new DeleteItemPlanController(deleteItemPlanUseCase)
+}
