1cc6967f465a27c07b3a87bcbcdec7b93609da83
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 16:48:48 -0300

    feat: delete item signature controller

---
+++ src/presentation/controllers/signature/delete-item.ts
@@ -0,0 +1,23 @@
+import { DeleteItemSignatureUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class DeleteItemSignatureController implements Controller {
+	private readonly deleteItemSignatureUseCase: DeleteItemSignatureUseCase
+
+	constructor(deleteItemSignatureUseCase: DeleteItemSignatureUseCase) {
+		this.deleteItemSignatureUseCase = deleteItemSignatureUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const { idSubscription, idItem } = httpRequest.params
+
+			const itemSignature = await this.deleteItemSignatureUseCase.delete(idSubscription, idItem)
+
+			return success(itemSignature)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
