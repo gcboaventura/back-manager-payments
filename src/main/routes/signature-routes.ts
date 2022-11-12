b233fbf3eb2a8baf52e2af7ba1deba2112fb6ead
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:45:54 -0300

    feat: list signatures route

--- src/main/routes/signature-routes.ts
+++ src/main/routes/signature-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters'
-import { addSignatureFactory, getSignatureFactory } from '../factories'
+import { addSignatureFactory, getSignatureFactory, listSignaturesFactory } from '../factories'
 
 export default (router: Router): void => {
 	router.post('/subscriptions', adaptRoute(addSignatureFactory()))
 
 	router.get('/subscriptions/:id', adaptRoute(getSignatureFactory()))
+
+	router.get('/subscriptions', adaptRoute(listSignaturesFactory()))
 }
