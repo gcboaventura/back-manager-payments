97d0ec55eacac4f6d1f573f0c1c1e3b9dd0b1cb5
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 17:50:14 -0300

    feat: delete item plan route

--- src/main/routes/plan-routes.ts
+++ src/main/routes/plan-routes.ts
@@ -7,7 +7,8 @@ import {
 	deletePlanFactory,
 	listPlansFactory,
 	addItemPlanFactory,
-	updateItemPlanFactory
+	updateItemPlanFactory,
+	deleteItemPlanFactory
 } from '../factories'
 
 export default (router: Router): void => {
@@ -24,4 +25,6 @@ export default (router: Router): void => {
 	router.post('/plans/:id/items', adaptRoute(addItemPlanFactory()))
 
 	router.put('/plans/:idPlan/items/:idItem', adaptRoute(updateItemPlanFactory()))
+
+	router.delete('/plans/:idPlan/items/:idItem', adaptRoute(deleteItemPlanFactory()))
 }
