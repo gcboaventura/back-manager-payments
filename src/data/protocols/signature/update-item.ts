349628f2ec9d24ea09075c4a1b892fc0114f311b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 16:23:13 -0300

    feat: update item signature data layer

---
+++ src/data/protocols/signature/update-item.ts
@@ -0,0 +1,5 @@
+import { UpdateItemSignatureUseCase } from '../../../domain'
+
+export interface UpdateItemSignatureGateway extends UpdateItemSignatureUseCase {}
+
+export interface UpdateItemSignatureRepository extends UpdateItemSignatureUseCase {}
---
+++ src/data/signature/update-item.ts
@@ -0,0 +1,35 @@
+import { UpdateItemSignatureGateway, UpdateItemSignatureRepository } from '../protocols'
+import {
+	UpdateItemSignatureUseCase,
+	ItemSignature,
+	ResponseHandleItemSignature
+} from '../../domain'
+
+export class UpdateItemSignatureData implements UpdateItemSignatureUseCase {
+	private readonly updateItemSignatureGateway: UpdateItemSignatureGateway
+	private readonly updateItemSignatureRepository: UpdateItemSignatureRepository
+
+	constructor(
+		updateItemSignatureGateway: UpdateItemSignatureGateway,
+		updateItemSignatureRepository: UpdateItemSignatureRepository
+	) {
+		this.updateItemSignatureGateway = updateItemSignatureGateway
+		this.updateItemSignatureRepository = updateItemSignatureRepository
+	}
+
+	async update(
+		subscription_id: string,
+		item_id: string,
+		item: ItemSignature
+	): Promise<ResponseHandleItemSignature> {
+		const updateGateway = await this.updateItemSignatureGateway.update(
+			subscription_id,
+			item_id,
+			item
+		)
+
+		await this.updateItemSignatureRepository.update(subscription_id, item_id, item)
+
+		return updateGateway
+	}
+}
