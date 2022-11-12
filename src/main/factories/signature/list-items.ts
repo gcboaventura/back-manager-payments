53e9888b6a9f85a32b053f61793eb596d4cd943c
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 16:13:04 -0300

    feat: list items signature factory

---
+++ src/main/factories/signature/list-items.ts
@@ -0,0 +1,11 @@
+import { ListItemsSignatureData } from '../../../data'
+import { ListItemsSignatureController } from '../../../presentation'
+import { ListItemsSignaturePagarme, AXIOS } from '../../../infra'
+
+export const listItemsSignatureFactory = (): ListItemsSignatureController => {
+	const listItemsSignatureGateway = new ListItemsSignaturePagarme(AXIOS)
+
+	const listItemsSignatureUseCase = new ListItemsSignatureData(listItemsSignatureGateway)
+
+	return new ListItemsSignatureController(listItemsSignatureUseCase)
+}
