bea047b7701d55ba4a9b70fa41e04910c8b5945b
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 16:33:59 -0300

    feat: update items signature factory

---
+++ src/main/factories/signature/update-item.ts
@@ -0,0 +1,24 @@
+import { UpdateItemSignatureData } from '../../../data'
+import { UpdateItemSignatureController } from '../../../presentation'
+import { DateUtils } from '../../../utils'
+import {
+	UpdateItemSignaturePagarme,
+	UpdateItemsSignatureMysql,
+	AXIOS,
+	connection
+} from '../../../infra'
+
+export const UpdateItemSignatureFactory = (): UpdateItemSignatureController => {
+	const UpdateItemSignatureGateway = new UpdateItemSignaturePagarme(AXIOS)
+
+	const dateUtils = new DateUtils()
+
+	const updateItemsSignatureRepository = new UpdateItemsSignatureMysql(connection, dateUtils)
+
+	const updateItemSignatureUseCase = new UpdateItemSignatureData(
+		UpdateItemSignatureGateway,
+		updateItemsSignatureRepository
+	)
+
+	return new UpdateItemSignatureController(updateItemSignatureUseCase)
+}
