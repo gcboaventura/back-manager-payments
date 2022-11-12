cf48fb3fbd2f68c049a4cf38d259807c79b8c34a
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 17:44:41 -0300

    feat: delete item plan data layer

---
+++ src/data/plan/delete-item.ts
@@ -0,0 +1,23 @@
+import { DeleteItemPlanUseCase, Items, ResponseHandleItemsPlans } from '../../domain'
+import { DeleteItemPlanGateway, DeleteItemPlanRepository } from '../protocols'
+
+export class DeleteItemPlanData implements DeleteItemPlanUseCase {
+	private readonly deleteItemPlanGateway: DeleteItemPlanGateway
+	private readonly deleteItemPlanRepository: DeleteItemPlanRepository
+
+	constructor(
+		deleteItemPlanGateway: DeleteItemPlanGateway,
+		deleteItemPlanRepository: DeleteItemPlanRepository
+	) {
+		this.deleteItemPlanGateway = deleteItemPlanGateway
+		this.deleteItemPlanRepository = deleteItemPlanRepository
+	}
+
+	async delete(plan_id: string, item_id: string): Promise<ResponseHandleItemsPlans> {
+		const addGateway = await this.deleteItemPlanGateway.delete(plan_id, item_id)
+
+		await this.deleteItemPlanRepository.delete(plan_id, item_id)
+
+		return addGateway
+	}
+}
---
+++ src/data/protocols/plan/delete-item.ts
@@ -0,0 +1,5 @@
+import { DeleteItemPlanUseCase } from '../../../domain'
+
+export interface DeleteItemPlanGateway extends DeleteItemPlanUseCase {}
+
+export interface DeleteItemPlanRepository extends DeleteItemPlanUseCase {}
