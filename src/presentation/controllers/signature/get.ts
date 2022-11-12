14d3af4500f90c9fc4dd1bb1a9ccc265d52bdb17
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:30:18 -0300

    feat: get signature controller

---
+++ src/presentation/controllers/signature/get.ts
@@ -0,0 +1,21 @@
+import { GetSignatureUseCase } from '../../../domain'
+import { serverError, success } from '../../helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class GetSignatureController implements Controller {
+	private readonly getSignatureUseCase: GetSignatureUseCase
+
+	constructor(getSignatureUseCase: GetSignatureUseCase) {
+		this.getSignatureUseCase = getSignatureUseCase
+	}
+
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		try {
+			const signature = await this.getSignatureUseCase.get(httpRequest.params.id)
+
+			return success(signature)
+		} catch (error: unknown) {
+			return serverError(error as Error)
+		}
+	}
+}
