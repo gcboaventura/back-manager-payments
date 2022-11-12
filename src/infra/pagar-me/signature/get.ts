17b8c4608a043a84c3ee4445d0a61fa4040b2821
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:33:56 -0300

    feat: get signature pagarme

---
+++ src/infra/pagar-me/signature/get.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { GetSignatureGateway } from '../../../data'
+import { SignatureModel } from '../../../domain'
+
+export class GetSignaturePagarme implements GetSignatureGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async get(signature_id: string): Promise<SignatureModel[]> {
+		const { data } = await this.axios.get(`/subscriptions/${signature_id}`)
+		return data
+	}
+}
