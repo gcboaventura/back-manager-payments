76cb09c345b433a6c4b727745e4c25d62a332e0a
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 17:32:56 -0300

    feat: update item plan pagarme

---
+++ src/infra/pagar-me/plan/update-item.ts
@@ -0,0 +1,20 @@
+import { Axios } from 'axios'
+import { UpdateItemPlanGateway } from '../../../data'
+import { ItemModelPlan, ResponseHandleItemsPlans } from '../../../domain'
+
+export class UpdateItemPlanPagarme implements UpdateItemPlanGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async update(
+		plan_id: string,
+		item_id: string,
+		item: ItemModelPlan
+	): Promise<ResponseHandleItemsPlans> {
+		const { data } = await this.axios.put(`/plans/${plan_id}/items/${item_id}`, item)
+		return data
+	}
+}
