83749d589cbca99b14567776573a256b2a717016
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 14:56:16 -0300

    feat: cancel signature factory

---
+++ src/main/factories/signature/cancel.ts
@@ -0,0 +1,19 @@
+import { CancelSignatureData } from '../../../data'
+import { CancelSignatureController } from '../../../presentation'
+import { DateUtils } from '../../../utils'
+import { CancelSignatureMysql, CancelSignaturePagarme, AXIOS, connection } from '../../../infra'
+
+export const cancelSignatureFactory = (): CancelSignatureController => {
+	const cancelSignatureGateway = new CancelSignaturePagarme(AXIOS)
+
+	const dateUtils = new DateUtils()
+
+	const cancelSignatureRepository = new CancelSignatureMysql(connection, dateUtils)
+
+	const cancelSignatureUseCase = new CancelSignatureData(
+		cancelSignatureGateway,
+		cancelSignatureRepository
+	)
+
+	return new CancelSignatureController(cancelSignatureUseCase)
+}
