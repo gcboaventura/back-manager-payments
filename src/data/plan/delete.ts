380ed6272ed96408d955cc19909e2e341b40eb53
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 18:25:59 -0300

    feat: delete plans and items

--- src/data/plan/delete.ts
+++ src/data/plan/delete.ts
@@ -1,19 +1,41 @@
-import { DeletePlanUseCase, PlanModel } from '../../domain'
-import { DeletePlanGateway, DeletePlanRepository } from '../protocols'
+import { DeletePlanUseCase, ItemModelPlan, PlanModel } from '../../domain'
+import {
+	DeleteItemPlanRepository,
+	DeletePlanGateway,
+	DeletePlanRepository,
+	GetPlanGateway
+} from '../protocols'
 
 export class DeletePlanData implements DeletePlanUseCase {
 	private readonly deletePlanGateway: DeletePlanGateway
 	private readonly deletePlanRepository: DeletePlanRepository
+	private readonly getPlanGateway: GetPlanGateway
+	private readonly deleteItemsPlanRepository: DeleteItemPlanRepository
 
-	constructor(deletePlanGateway: DeletePlanGateway, deletePlanRepository: DeletePlanRepository) {
+	constructor(
+		deletePlanGateway: DeletePlanGateway,
+		deletePlanRepository: DeletePlanRepository,
+		getPlanGateway: GetPlanGateway,
+		deleteItemsPlanRepository: DeleteItemPlanRepository
+	) {
 		this.deletePlanGateway = deletePlanGateway
 		this.deletePlanRepository = deletePlanRepository
+		this.getPlanGateway = getPlanGateway
+		this.deleteItemsPlanRepository = deleteItemsPlanRepository
 	}
 
 	async delete(plan_id: string): Promise<PlanModel> {
+		const deletedPlan = await this.getPlanGateway.get(plan_id)
+
 		const deleteGateway = await this.deletePlanGateway.delete(plan_id)
 
 		await this.deletePlanRepository.delete(plan_id, deleteGateway.deleted_at)
+
+		const deleteItems = deletedPlan.items.map(async (x: ItemModelPlan) => {
+			await this.deleteItemsPlanRepository.delete(deleteGateway.deleted_at, x.id)
+		})
+
+		await Promise.all(deleteItems)
 
 		return deleteGateway
 	}
--- src/main/factories/plan/delete.ts
+++ src/main/factories/plan/delete.ts
@@ -1,7 +1,14 @@
+import { DateUtils } from '../../../utils'
 import { DeletePlanData } from '../../../data'
 import { DeletePlanController } from '../../../presentation'
-import { DeletePlanMysql, DeletePlanPagarme, AXIOS, connection } from '../../../infra'
-import { DateUtils } from '../../../utils'
+import {
+	DeletePlanMysql,
+	DeletePlanPagarme,
+	AXIOS,
+	connection,
+	DeleteItemPlanMysql,
+	GetPlanPagarme
+} from '../../../infra'
 
 export const deletePlanFactory = (): DeletePlanController => {
 	const deletePlanGateway = new DeletePlanPagarme(AXIOS)
@@ -10,7 +17,16 @@ export const deletePlanFactory = (): DeletePlanController => {
 
 	const deletePlanRepository = new DeletePlanMysql(connection, dateUtils)
 
-	const deletePlanUseCase = new DeletePlanData(deletePlanGateway, deletePlanRepository)
+	const getPlanGateway = new GetPlanPagarme(AXIOS)
+
+	const deleteItemsPlanRepository = new DeleteItemPlanMysql(connection, dateUtils)
+
+	const deletePlanUseCase = new DeletePlanData(
+		deletePlanGateway,
+		deletePlanRepository,
+		getPlanGateway,
+		deleteItemsPlanRepository
+	)
 
 	return new DeletePlanController(deletePlanUseCase)
 }
