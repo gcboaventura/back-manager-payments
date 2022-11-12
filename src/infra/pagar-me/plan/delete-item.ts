8e0b90510215b4696a770c7028cb75ce354f88f6
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 17:45:53 -0300

    feat: delete item plan pagarme

---
+++ src/infra/pagar-me/plan/delete-item.ts
@@ -0,0 +1,16 @@
+import { Axios } from 'axios'
+import { DeleteItemPlanGateway } from '../../../data'
+import { ResponseHandleItemsPlans } from '../../../domain'
+
+export class DeleteItemPlanPagarme implements DeleteItemPlanGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async delete(plan_id: string, item_id: string): Promise<ResponseHandleItemsPlans> {
+		const { data } = await this.axios.delete(`/plans/${plan_id}/items/${item_id}`)
+		return data
+	}
+}
