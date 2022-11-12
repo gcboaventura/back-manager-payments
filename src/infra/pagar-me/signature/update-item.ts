4cbe2a065e8ef8fddea85cfa7a2a6fb68d1da9ca
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 16:24:51 -0300

    feat: update item signature pagarme

---
+++ src/infra/pagar-me/signature/update-item.ts
@@ -0,0 +1,23 @@
+import { Axios } from 'axios'
+import { UpdateItemSignatureGateway } from '../../../data'
+import { ItemSignature, ResponseHandleItemSignature } from '../../../domain'
+
+export class UpdateItemSignaturePagarme implements UpdateItemSignatureGateway {
+	private readonly axios: Axios
+
+	constructor(axios: Axios) {
+		this.axios = axios
+	}
+
+	async update(
+		subscription_id: string,
+		item_id: string,
+		item: ItemSignature
+	): Promise<ResponseHandleItemSignature> {
+		const { data } = await this.axios.post(
+			`/subscriptions/${subscription_id}/items/${item_id}`,
+			item
+		)
+		return data
+	}
+}
