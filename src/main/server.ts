f4d1bba51d679b29dcd4349ce8985998936c3bab
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 17:22:11 -0300

    refactor: remove console log

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,7 +2,6 @@ import { app, env } from './config'
 import { connection, CreateTable } from '../infra/database'
 
 connection.connect((error: any) => {
-	console.log(error)
 	if (!error) {
 		new CreateTable(connection)
 		app.listen(env.PORT, () => console.log(`server running at http://localhost:${env.PORT}`))
