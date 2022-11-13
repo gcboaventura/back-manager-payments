6071fa320a41cfeddc2c0f3bb3a0ecc974b7f35e
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 13:20:23 -0300

    feat: use enum

--- src/main/routes/health-check-routes.ts
+++ src/main/routes/health-check-routes.ts
@@ -1,7 +1,8 @@
 import { Response, Router, Request } from 'express'
+import { StatusCode } from '../../domain'
 import { env } from '../config'
 export default (router: Router): void => {
 	router.get('/health-check', (request: Request, response: Response) =>
-		response.status(200).json({ check: `I'm working fine, on port ${env.PORT}.` })
+		response.status(StatusCode.Success).json({ check: `I'm working fine, on port ${env.PORT}.` })
 	)
 }
