7097d68e59dbcc75e5302c2e9126188f6bdd2019
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 21:43:43 -0300

    feat: get device repository

---
+++ src/infra/database/device/get.ts
@@ -0,0 +1,22 @@
+import { Connection } from 'mysql'
+import { GetDeviceRepository } from '../../../data'
+import { DeviceModel } from '../../../domain'
+
+export class GetDeviceMysql implements GetDeviceRepository {
+	private readonly connection: Connection
+
+	constructor(connection: Connection) {
+		this.connection = connection
+	}
+
+	async get(id: number): Promise<DeviceModel> {
+		return new Promise((resolve, reject) => {
+			this.connection.query(`SELECT * FROM DEVICES WHERE id = ? `, [id], (error: any, res: any) => {
+				if (error) {
+					reject(error)
+				}
+				resolve(res)
+			})
+		})
+	}
+}
