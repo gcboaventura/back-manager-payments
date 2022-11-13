7d5093a877894083eb4b7279fcbf546be9406088
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 13:17:44 -0300

    feat: object helth check

--- src/main/routes/health-check-routes.ts
+++ src/main/routes/health-check-routes.ts
@@ -2,6 +2,6 @@ import { Response, Router, Request } from 'express'
 import { env } from '../config'
 export default (router: Router): void => {
 	router.get('/health-check', (request: Request, response: Response) =>
-		response.status(200).json(`I'm working fine, on port ${env.PORT}.`)
+		response.status(200).json({ check: `I'm working fine, on port ${env.PORT}.` })
 	)
 }
