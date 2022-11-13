de05a223c32e7124af096306ac87b13b4342cb38
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 17:12:51 -0300

    refactor: revert connection

--- src/infra/database/config/connection.ts
+++ src/infra/database/config/connection.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql'
 import { env } from '../../../main/config'
 
-export const connection = mysql.createPool({
+export const connection = mysql.createConnection({
 	host: env.DB_HOST,
 	user: env.DB_USER,
 	password: env.DB_PASSWORD,
