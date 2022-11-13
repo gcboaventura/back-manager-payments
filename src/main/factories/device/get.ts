36d41e3b72f894946e8f172d0c7a9b71ecabc2ae
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 21:44:41 -0300

    feat: get device factory

---
+++ src/main/factories/device/get.ts
@@ -0,0 +1,11 @@
+import { GetDeviceData } from '../../../data'
+import { GetDeviceController } from '../../../presentation'
+import { GetDeviceMysql, connection } from '../../../infra'
+
+export const getDeviceFactory = (): GetDeviceController => {
+	const getDeviceRepository = new GetDeviceMysql(connection)
+
+	const getDeviceUseCase = new GetDeviceData(getDeviceRepository)
+
+	return new GetDeviceController(getDeviceUseCase)
+}
