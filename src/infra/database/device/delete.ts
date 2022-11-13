9da9bef69e8caaaca8c614ad48cb263b7df9c517
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 23:16:54 -0300

    feat: delete device repository

---
+++ src/infra/database/device/delete.ts
@@ -0,0 +1,22 @@
+import { Connection } from 'mysql'
+import { DeleteDeviceRepository } from '../../../data'
+import { DeviceModel } from '../../../domain'
+
+export class DeleteDeviceMysql implements DeleteDeviceRepository {
+	private readonly connection: Connection
+
+	constructor(connection: Connection) {
+		this.connection = connection
+	}
+
+	async delete(id: number): Promise<DeviceModel> {
+		return new Promise((resolve, reject) => {
+			this.connection.query(`DELETE DEVICES WHERE id = ?`, [id], (error: any, res: any) => {
+				if (error) {
+					reject(error)
+				}
+				resolve(res)
+			})
+		})
+	}
+}
