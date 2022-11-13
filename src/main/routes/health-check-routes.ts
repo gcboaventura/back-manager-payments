9e2d6117eb9b04dfcb30f53803d6077fa454eca8
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 13:16:27 -0300

    feat: helth check route

---
+++ src/main/routes/health-check-routes.ts
@@ -0,0 +1,7 @@
+import { Response, Router, Request } from 'express'
+import { env } from '../config'
+export default (router: Router): void => {
+	router.get('/health-check', (request: Request, response: Response) =>
+		response.status(200).json(`I'm working fine, on port ${env.PORT}.`)
+	)
+}
