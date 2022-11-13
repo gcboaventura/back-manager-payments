4dfeed97a27f821e8f3f3271ca5cde1e4917db80
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 16:18:58 -0300

    feat: adds space

--- src/main/routes/health-check-routes.ts
+++ src/main/routes/health-check-routes.ts
@@ -1,6 +1,7 @@
 import { Response, Router, Request } from 'express'
 import { StatusCode } from '../../domain'
 import { env } from '../config'
+
 export default (router: Router): void => {
 	router.get('/health-check', (request: Request, response: Response) =>
 		response.status(StatusCode.Success).json({ check: `I'm working fine, on port ${env.PORT}.` })
