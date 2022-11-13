77beae8c208b892c2dbb3ab2901589f591db93b9
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-12 21:32:01 -0300

    feat: add device repository

---
+++ src/infra/database/device/add.ts
@@ -0,0 +1,26 @@
+import { Connection } from 'mysql'
+import { AddDeviceRepository } from '../../../data'
+import { Device, DeviceModel } from '../../../domain'
+
+export class AddDeviceMysql implements AddDeviceRepository {
+	private readonly connection: Connection
+
+	constructor(connection: Connection) {
+		this.connection = connection
+	}
+
+	async add(device: Device): Promise<DeviceModel> {
+		return new Promise((resolve, reject) => {
+			this.connection.query(
+				`INSERT INTO DEVICES (name, full_price) VALUES (?, ?)`,
+				[device.name, device.full_price],
+				(error: any, res: any) => {
+					if (error) {
+						reject(error)
+					}
+					resolve(res)
+				}
+			)
+		})
+	}
+}
