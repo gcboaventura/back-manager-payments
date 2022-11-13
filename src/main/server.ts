d1b0fd4382aa78a5d4a51d12276c51fb366908c6
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-13 17:07:35 -0300

    feat: console error database

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,6 +2,7 @@ import { app, env } from './config'
 import { connection, CreateTable } from '../infra/database'
 
 connection.connect((error: any) => {
+	console.log(error)
 	if (!error) {
 		new CreateTable(connection)
 		app.listen(env.PORT, () => console.log(`server running at http://localhost:${env.PORT}`))
