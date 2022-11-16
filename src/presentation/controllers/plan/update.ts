5394b260c78bdd13b51033e7a06bc8a75d06ac98
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-16 13:17:53 -0300

    feat: required fields update plan

--- src/presentation/controllers/plan/update.ts
+++ src/presentation/controllers/plan/update.ts
@@ -1,5 +1,6 @@
 import { UpdatePlanUseCase } from '../../../domain'
-import { serverError, success } from '../../helpers'
+import { MissingParamError } from '../../errors'
+import { badRequest, serverError, success } from '../../helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class UpdatePlanController implements Controller {
@@ -11,6 +12,26 @@ export class UpdatePlanController implements Controller {
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		try {
+			const requiredFields: string[] = [
+				'name',
+				'description',
+				'payment_methods',
+				'installments',
+				'minimum_price',
+				'statement_descriptor',
+				'currency',
+				'interval',
+				'trial_period_days',
+				'billing_type',
+				'items'
+			]
+
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field))
+				}
+			}
+
 			const plan = await this.updatePlanUseCase.update(httpRequest.body, httpRequest.params.id)
 
 			return success(plan)
