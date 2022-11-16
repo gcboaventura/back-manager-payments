7fd12960a2812ec05509a6322ea351fe19835bca
Guilherme de Carvalho Boaventura <guilherme.boaventura@involucro.com.br>
2022-11-16 13:16:08 -0300

    feat: required fields plan

--- src/presentation/controllers/plan/add.ts
+++ src/presentation/controllers/plan/add.ts
@@ -1,5 +1,6 @@
 import { AddPlanUseCase } from '../../../domain'
-import { serverError, success } from '../../helpers'
+import { MissingParamError } from '../../errors'
+import { badRequest, serverError, success } from '../../helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 
 export class AddPlanController implements Controller {
@@ -11,6 +12,26 @@ export class AddPlanController implements Controller {
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		try {
+			const requiredFields: string[] = [
+				'name',
+				'description',
+				'payment_methods',
+				'installments',
+				'minimum_price',
+				'statement_descriptor',
+				'currency',
+				'interval',
+				'trial_period_days',
+				'billing_type',
+				'items'
+			]
+
+			for (const field of requiredFields) {
+				if (!httpRequest.body[field]) {
+					return badRequest(new MissingParamError(field))
+				}
+			}
+
 			const plan = await this.addPlanUseCase.add(httpRequest.body)
 
 			return success(plan)
